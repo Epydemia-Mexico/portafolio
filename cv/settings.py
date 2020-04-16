a6882506ed6da80fd93d2006d4bfa4cdabfbc3fd
ChristianBos <christianbos91@gmail.com>
2020-04-15 22:50:12 -0500

    Proyecto terminado, se agregan vistas para cada modelo, se crea el archivo base para el html y se agrega skills

--- cv/settings.py
+++ cv/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-mx'
 
 TIME_ZONE = 'UTC'
 
@@ -127,3 +127,6 @@ STATICFILES_DIRS = [
 
 # Unicamente usar en un entorno productivo.
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, MEDIA_URL)
--- cv/urls.py
+++ cv/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
-from dashboard.views import Index
+from dashboard.views import Index, SkillsView, EducationView, WorkExperienceView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     #  Siempre que usemos clases basadas en vista debemos utilizar el método .as_view() de la clase.
     path('', Index.as_view()),
-]
+    path('habilidades', SkillsView.as_view(), name='habilidades'),
+    path('educacion', EducationView.as_view(), name='edu'),
+    path('trabajos', WorkExperienceView.as_view(), name='works'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
